6ae13b800ddcfcc347213fbd6c3d88bbeac6185c
Joe Lavond <joelavond@gmail.com>
2023-03-22 14:30:09 +0000

    ready to run alt methods on femnist and pubmed

---
+++ custom/femnist--s02/pfedme/helper_sweep_pfedme.sh
@@ -0,0 +1,26 @@
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+done
+
+python federatedscope/main.py \
+    --cfg custom/femnist--s02/base.yaml \
+    outdir 'custom/femnist--s02/wandb' \
+    wandb.use True \
+    wandb.name_project 'decay' \
+    wandb.name_user 'joelavond' \
+    federate.method 'pFedMe' \
+    personalization.lr '-1.0' \
+    expname femnist--s02--n_epochs${local_update_steps}--batch_size${batch_size}--lr${lr}--regular_weight${regular_weight}--K${K} \
+    federate.local_update_steps ${local_update_steps} \
+    optimizer.lr ${lr} \
+    data.batch_size ${batch_size} \
+    personalization.regular_weight ${regular_weight} \
+    personalization.K ${K}
+
+
---
+++ custom/pubmed/fedavg/helper_sweep_fedavg_finetune.sh
@@ -0,0 +1,19 @@
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+done
+
+python federatedscope/main.py \
+    --cfg custom/pubmed/base_finetune.yaml \
+    outdir 'custom/pubmed/wandb' \
+    wandb.use True \
+    wandb.name_user 'joelavond' \
+    expname pubmed--n_epochs${local_update_steps}--lr${lr} \
+    federate.local_update_steps ${local_update_steps} \
+    optimizer.lr ${lr}
+
