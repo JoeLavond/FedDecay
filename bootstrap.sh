9e2c58e10c9f080f83c4d76ecae293b6f791c5ea
JoeLavond <joelavond@gmail.com>
2023-08-15 12:01:46 -0400

    need to remove any excess changes to codebase and remove authorship

--- bootstrap.sh
+++
@@ -1,7 +0,0 @@
- # remove secure connections
-  echo "Run '. ./bootstrap.sh' to run in current shell"
-  export http_proxy=http://152.2.41.28:3128
-  export HTTP_PROXY=http://152.2.41.28:3128
-  export https_proxy=http://152.2.41.28:3128
-  export HTTPS_PROXY=http://152.2.41.28:3128
-  echo "Verify echo \$https_proxy=$https_proxy is not secure"
--- unit_test_agent_sweep.sh
+++
@@ -1,6 +0,0 @@
-bash -c "
-    source activate fs;
-    CUDA_VISIBLE_DEVICES=0 wandb agent joelavond/${1};
-    sleep 300;
-"
-
